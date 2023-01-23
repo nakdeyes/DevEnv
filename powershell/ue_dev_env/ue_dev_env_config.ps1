b2eaa9660e2abca3b4e062a2cd1c4b657b8fa310
nakedeyes <timothy.rawcliffe@gmail.com>
2023-01-22 23:05:29 -0800

    DevEnv - Powershell - add git powershell dev env directory to support push/pull from git directory.

--- powershell/ue_dev_env/ue_dev_env_config.ps1
+++ powershell/ue_dev_env/ue_dev_env_config.ps1
@@ -20,10 +20,6 @@ $global:ProfileConfig = New-Object System.Object
 $global:ProfileConfig | Add-Member -type NoteProperty -name "P4ParallelSyncThreads" -value 12
 
 # Misc App paths
-$global:AppPaths = New-Object System.Object
-$global:AppPaths | Add-Member -type NoteProperty -name "Rider" -value "C:\Program Files\JetBrains\JetBrains Rider 2022.2.4\bin\rider64.exe"
-$global:AppPaths | Add-Member -type NoteProperty -name "TextEditor" -value "nvim"
-$global:AppPaths | Add-Member -type NoteProperty -name "UnrealGameSync" -value "C:\Program Files (x86)\UnrealGameSync\UnrealGameSyncLauncher.exe"
 $global:AppPaths | Add-Member -type NoteProperty -name "VisualStudio" -value "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe"
 
 
